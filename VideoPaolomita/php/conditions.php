837cdcb604b2426168452c95c0954a1f2d28d334
Valentina <Valentina@Vale>
2015-06-17 01:01:40 +0200

    end

--- VideoPaolomita/php/conditions.php
+++ VideoPaolomita/php/conditions.php
@@ -1,34 +1,34 @@
-<h1> Condiciones de venta </ ??h1>
+<h1> Condiciones de venta </h1>
 
-<p> Estos TÈrminos son para la compra de productos y servicios, llevado a cabo de forma remota a travÈs de la red inform·tica en "VideoPalomita.es". Cada transacciÛn de compra se regir· por las disposiciones del Decreto n. 185/99 y, en cuanto a la protecciÛn de la confidencialidad, que se presentar· a las reglas de la Ley n. 675/96 y sus modificaciones. </ P>
+<p> Estos Terminos son para la compra de productos y servicios, llevado a cabo de forma remota a trav√©s de la red inform√°tica en "VideoPalomita.es". Cada transacci√≥n de compra se regir√≥ por las disposiciones del Decreto n. 185/99 y, en cuanto a la protecci√≥n de la confidencialidad, que se presentar√° a las reglas de la Ley n. 675/96 y sus modificaciones. </p>
 
-<h3 class = 'h3-cond> AceptaciÛn de las condiciones generales de venta </ ??h3>
-<p> El cliente, mediante el envÌo de la confirmaciÛn electrÛnica de su orden de compra, acepta incondicionalmente y se compromete a cumplir en sus relaciones con VideoPalomita, las condiciones generales y el pago transcribe a continuaciÛn, declarando que he leÌdo y aceptado toda la informaciÛn proporcionada a Èl en virtud de las normas antes mencionadas, y tambiÈn reconociendo que VideoPalomita.es no se considera obligado a las diferentes condiciones menos que se acuerde previamente por escrito. </ p>
+<h3 class = 'h3-cond> Aceptaci√≥n de las condiciones generales de venta </ ??h3>
+<p> El cliente, mediante el env√≠o de la confirmaci√≥n electr√≥nica de su orden de compra, acepta incondicionalmente y se compromete a cumplir en sus relaciones con VideoPalomita, las condiciones generales y el pago transcribe a continuaci√≥n, declarando que he le√≠do y aceptado toda la informaci√≥n proporcionada a √©l en virtud de las normas antes mencionadas, y tambi√©n reconociendo que VideoPalomita.es no se considera obligado a las diferentes condiciones menos que se acuerde previamente por escrito. </p>
 
-<h3 class = 'h3-cond> obligaciones del Cliente </ h3>
-<p> Estas condiciones de venta deben ser examinados por los visitantes en lÌnea clientes VideoPalomita.es antes de que confirme su compra. La presentaciÛn de la confirmaciÛn del pedido implica el pleno conocimiento de las Condiciones Generales de Venta y su plena aceptaciÛn. El cliente est· de acuerdo y se compromete, una vez que el proceso de compra en lÌnea, imprimir y guardar estas condiciones generales de venta, ya examinada y aceptada durante el proceso de compra, con el fin de satisfacer plenamente las condiciones de que al arte. 3 y 4 del Decreto Legislativo n. 185/1999. </ P>
+<h3 class = 'h3-cond> obligaciones del Cliente </h3>
+<p> Estas condiciones de venta deben ser examinados por los visitantes en l√≠nea clientes VideoPalomita.es antes de que confirme su compra. La presentaci√≥n de la confirmaci√≥n del pedido implica el pleno conocimiento de las Condiciones Generales de Venta y su plena aceptaci√≥n. El cliente est√≥ de acuerdo y se compromete, una vez que el proceso de compra en l√≠nea, imprimir y guardar estas condiciones generales de venta, ya examinada y aceptada durante el proceso de compra, con el fin de satisfacer plenamente las condiciones de que al arte. 3 y 4 del Decreto Legislativo n. 185/1999. </ P>
 
-<h3 class = 'h3-cond'> Los precios de venta </ ??h3>
-<p> Los precios de lista publicados en la tienda en lÌnea incluyen IVA y est·n sujetos a cambios en cualquier momento sin VideoPalomita debe notificar a ninguna, se exime de cualquier responsabilidad relacionada con cualquier descripciÛn incorrecta. </ P>
-<p> Esta informaciÛn ser· enviada a la direcciÛn de correo electrÛnico proporcionada por el consumidor tambiÈn en el momento de la orden, junto con la informaciÛn de la orden de entrega o cualquier otro gasto. La confirmaciÛn final de la orden se entregar· a usted el cliente, una vez m·s por e-mail sobre la terminaciÛn de la transacciÛn y contendr· la misma informaciÛn </ p>
+<h3 class = 'h3-cond'> Los precios de venta </h3>
+<p> Los precios de lista publicados en la tienda en l√≠nea incluyen IVA y est√°n sujetos a cambios en cualquier momento sin VideoPalomita debe notificar a ninguna, se exime de cualquier responsabilidad relacionada con cualquier descripci√≥n incorrecta. </ P>
+<p> Esta informaci√≥n ser√° enviada a la direcci√≥n de correo electr√≥nico proporcionada por el consumidor tambi√©n en el momento de la orden, junto con la informaci√≥n de la orden de entrega o cualquier otro gasto. La confirmaci√≥n final de la orden se entregar√° a usted el cliente, una vez m√°s por e-mail sobre la terminaci√≥n de la transacci√≥n y contendr√° la misma informaci√≥n </ p>
 
 <h3 class = 'h3-cond> compra Modalidad </ h3>
-<p> VideoPalomita.es es un sitio web de venta de productos y software. Las compras de productos, disponibles, asÌ como ilustrados y descritos en lÌnea en sus respectivas p·ginas y tÈcnicas, es hecha por el cliente en el precio especificado en el mismo, para ser confirmado en el momento de la confirmaciÛn del pedido, adem·s de los costos de transporte de la siguiente manera especificada en el arte. 6, y todavÌa muestra el momento de la compra. </ P>
-<p> Antes de confirmar la compra, el cliente se mostrar· el costo unitario de cada producto elegido, el coste total cuando usted compra m·s productos y los costos de transporte relacionados. </ P>
-<p> ConfirmaciÛn de la adquisiciÛn, tras la finalizaciÛn de la transacciÛn, el cliente recibir· un mensaje de correo electrÛnico que contiene la fecha y el importe total de su pedido y los detalles de la misma. </ P>
+<p> VideoPalomita.es es un sitio web de venta de peliculas. Las compras de productos, disponibles, as√≠ como ilustrados y descritos en l√≠nea en sus respectivas p√°ginas y t√©cnicas, es hecha por el cliente en el precio especificado en el mismo, para ser confirmado en el momento de la confirmaci√≥n del pedido, adem√°s de los costos de transporte de la siguiente manera especificada en el arte. 6, y todav√≠a muestra el momento de la compra. </ P>
+<p> Antes de confirmar la compra, el cliente se mostrar√° el costo unitario de cada producto elegido, el coste total cuando usted compra m√≠s productos y los costos de transporte relacionados. </ P>
+<p> Confirmaci√≥n de la adquisici√≥n, tras la finalizaci√≥n de la transacci√≥n, el cliente recibir√° un mensaje de correo electr√≥nico que contiene la fecha y el importe total de su pedido y los detalles de la misma. </ P>
 
 <h3 class = 'h3-cond> Los costos de transporte </ h3>
-<p> El costo de envÌo se calcula autom·ticamente en funciÛn del costo de los productos adquiridos e incluye el seguro de mercancÌas en tr·nsito. </ P>
+<p> El costo de env√≠o se calcula autom√°ticamente en funci√≥n del costo de los productos adquiridos e incluye el seguro de mercanc√≠as en tr√°nsito. </ P>
 
-<h3 class = 'h3-cond> Ûrdenes de cancelaciÛn </ h3>
-<p> VideoPalomita.es se reserva la posibilidad de cancelar un pedido o una posiciÛn de pedido si alguna de las circunstancias siguientes: </ p>
+<h3 class = 'h3-cond> √ìrdenes de cancelaci√≥n </ h3>
+<p> VideoPalomita.es se reserva la posibilidad de cancelar un pedido o una posici√≥n de pedido si alguna de las circunstancias siguientes: </ p>
 <Clase ol = 'ol-cond>
-<li> artÌculos que se informan como agotado por las empresas distribuidoras; </ li>
-<li> artÌculos para problemas que no pueden ser entregados por los proveedores; </ li>
-<li> el fraude de tarjetas de crÈdito; </ li>
-<li> El uso incorrecto de los cupones. Sobre todo si por cualquier motivo la orden se somete a cancelaciones parciales que disminuirÌa la contraprestaciÛn total por debajo del mÌnimo necesario para la promociÛn o donde el uso de vales comprenden un descuento global de m·s del 20% de la orden, VideoPalomita .es lo har· unilateralmente a la cancelaciÛn del mismo descuento. </ li>
-<li> Precio cambios por parte del proveedor que implica cambios en los precios de m·s de 5%. Todos los pedidos est·n sujetos a la aceptaciÛn por el Vendedor que enviar· al cliente una aceptaciÛn o un rechazo de la orden en el menor tiempo posible. </ li>
-<li> El Vendedor se reserva el derecho, a su entera discreciÛn, el derecho de cancelar cualquier orden en caso de errores materiales asociados con el pedido del cliente o con la informaciÛn enviada por el cliente a travÈs de los www.VideoPalomita.es sitio, o en su caso inspecciÛn adicional de crÈdito del cliente sugiriÛ dicha cancelaciÛn. </ li>
-<li> FALTA DE PRESENTAR COPIA DEL RECIBO DE PAGO EN EL PLAZO DE 5 DÕAS DESPU…S DE LA FECHA DE LA ORDEN EN CASO DE REGLAS "DE BANCO DE PAGO </ li>
-<li> No-colecciÛn de productos bajo la orden en el plazo de 5 dÌas h·biles a partir de la comunicaciÛn de nuestros operadores en el caso de elecciÛn "en la retirada mediante el pago de dinero en efectivo". </ li>
+<li> art√≠culos que se informan como agotado por las empresas distribuidoras; </ li>
+<li> art√≠culos para problemas que no pueden ser entregados por los proveedores; </ li>
+<li> el fraude de tarjetas de cr√©dito; </ li>
+<li> El uso incorrecto de los cupones. Sobre todo si por cualquier motivo la orden se somete a cancelaciones parciales que disminuira la contraprestaci√≥n total por debajo del m√≠nimo necesario para la promoci√≥n o donde el uso de vales comprenden un descuento global de m√°s del 20% de la orden, VideoPalomita .es lo har√° unilateralmente a la cancelaci√≥n del mismo descuento. </ li>
+<li> Precio cambios por parte del proveedor que implica cambios en los precios de m√°s de 5%. Todos los pedidos est√°n sujetos a la aceptaci√≥n por el Vendedor que enviar√° al cliente una aceptaci√≥n o un rechazo de la orden en el menor tiempo posible. </ li>
+<li> El Vendedor se reserva el derecho, a su entera discreci√≥n, el derecho de cancelar cualquier orden en caso de errores materiales asociados con el pedido del cliente o con la informaci√≥n enviada por el cliente a trav√©s de los www.VideoPalomita.es sitio, o en su caso inspecci√≥n adicional de cr√©dito del cliente sugiri√≥ dicha cancelaci√≥n. </ li>
+<li> FALTA DE PRESENTAR COPIA DEL RECIBO DE PAGO EN EL PLAZO DE 5 D√çAS DESPU√âS DE LA FECHA DE LA ORDEN EN CASO DE REGLAS "DE BANCO DE PAGO </ li>
+<li> No-colecci√≥n de productos bajo la orden en el plazo de 5 d√≠as h√°biles a partir de la comunicaci√≥n de nuestros operadores en el caso de elecci√≥n "en la retirada mediante el pago de dinero en efectivo". </ li>
 </ ol>
