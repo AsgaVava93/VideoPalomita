c4714bf4096c9efd94b8a13cf999a4a1413c72b8
Valentina <Valentina@Vale>
2015-06-16 22:03:43 +0200

    SQL

--- VideoPaolomita/view/aside.php
+++ VideoPaolomita/view/aside.php
@@ -1,9 +1,9 @@
 <aside class="aside-nav fixed-nav">
 	<br>
 	<ul>
-		<li><a href='index.php?page=list&sc=desk' alt='Desktop'>Peliculas</a></li><!--
-                --><li><a href='index.php?page=list&sc=note' alt='notebook'>Nuevas</a></li><!--
-		--><li><a href='index.php?page=list&sc=net' alt='netbook'>Actores</a></li><!--
-                --><li><a href='index.php?page=list&sc=mon' alt='monitor'>Directores</a></li>
+		<li><a href='index.php?page=list&sc=peliculas' alt='Peliculas'>Peliculas</a></li><!--
+                --><li><a href='index.php?page=list&sc=nuevas' alt='Nuevas'>Nuevas</a></li><!--
+		--><li><a href='index.php?page=list&sc=actores' alt='Actores'>Actores</a></li><!--
+                --><li><a href='index.php?page=list&sc=directores' alt='Directores'>Directores</a></li>
 	</ul>
 </aside>
